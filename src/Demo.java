1ad224a9132db8bb5f86906fd004b1ebbdea9270
AdmiN <AdmiN@ITWS0029>
2024-08-27 11:24:16 +0530

    Commited

--- src/Demo.java
+++ src/Demo.java
@@ -2,5 +2,6 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hellos");
+		System.out.println("Welcome to LBRCE");
 	}
 }
